5f163b3db9e0f2d665883a86dbeddeefea4d14be
MeteorLee <meteorproject529@gamil.com>
2022-12-22 01:48:23 +0900

    Add: Add ex02

--- src/chapter03/array_list/ex01/Answer.java
+++ src/chapter03/array_list/ex01/Answer.java
@@ -17,10 +17,14 @@ public class Answer {
         }
 
         for (int i = 0; i < charArray.length; i++) {
-            sum += charArray[i] - '0';
+            sum += charArray[i] - '0'; // 48이라는 int 값이 아닌 '0'의 문자형을 빼준다.
         }
         bw.write(String.valueOf(sum));
         bw.flush();
         br.close();
     }
 }
+
+// 문자열 -> 문자형 배열[]로 변환 시 .toCharArray()를 사용하자.
+// 2, 5, 8과 같은 숫자로 이루어진 문자형을 int형으로 변환할 때 -48이 아닌
+// 문자형은 연산이 가능하기에 -'0'을 해주자
